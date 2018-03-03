343b6cf6403175c415fb1bee05f42cea76f685ff
DESKTOP-O58VQIE\Vidhya Prakash R <vidhyaprakash85@gmail.com>
2018-03-03 07:38:34 +0530

    Nav Bar Design Completed

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,7 +1,3 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+    Developed by Subha Team and Designed by <a href="https://adminlte.io">Almsaeed Studio</a>
 </footer>
