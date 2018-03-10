89371adfbc2e2352e031faaea63be1465f695381
RMK10810\RMKEC <ranj16308.cs@rmkec.ac.in>
2018-03-10 15:03:36 +0530

    change password language changed

--- application/language/hindi/message_lang.php
+++ application/language/hindi/message_lang.php
@@ -2,3 +2,21 @@
 
 $lang['mobile_number'] = 'मोबाइल नंबर';
 $lang['submit'] = 'जमा करें';
+$lang['name'] = 'नाम';
+$lang['address1'] = 'पता1';
+$lang['address2'] = 'पता2';
+$lang['city'] = 'शहर';
+$lang['state'] = 'राज्य';
+$lang['country'] = 'देश';
+$lang['aadhaar_Number'] = 'आधार संख्या';
+$lang['Email_id'] = 'ईमेल आईडी';
+$lang['old_password'] = 'पुराना पासवर्ड';
+$lang['new_password'] = 'नया पासवर्ड';
+$lang['confirmation_password'] = 'पुष्टिकरण पासवर्ड';
+$lang['select'] = 'चुनते हैं';
+$lang['change_password'] = 'पासवर्ड बदलें';
+$lang['edit_password'] = 'पासवर्ड संपादित करें';
+$lang['profile_change'] = 'प्रोफाइल परिवर्तन';
+$lang['editprofile'] = 'प्रोफाइल एडिट करें';
+$lang['personalinformation'] = 'व्यक्तिगत जानकारी';
+$lang['username'] = 'उपयोगकर्ता नाम';  
