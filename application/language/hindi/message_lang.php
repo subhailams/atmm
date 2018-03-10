59e263b5d4cc3b5ff72154b4cde328099b5b9164
RMK10808\RMKEC <subh16322.cs@rmkec.ac.in>
2018-03-10 14:57:44 +0530

    case register form hindi

--- application/language/hindi/message_lang.php
+++ application/language/hindi/message_lang.php
@@ -2,3 +2,14 @@
 
 $lang['mobile_number'] = 'मोबाइल नंबर';
 $lang['submit'] = 'जमा करें';
+
+/* case management form*/
+
+$lang['Registration Form'] = 'पंजीकरण फॉर्म';
+$lang['Case Details'] = 'केस विवरण';
+$lang['If others'] = 'अगर अन्य';
+$lang['Case Description'] ='केस विवरण';
+$lang['Date of Incident'] ='घटना की तिथि';
+$lang['Victim Name']='शिकार का नाम';
+
+
