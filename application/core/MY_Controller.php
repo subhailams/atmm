0a8375b9c9b4596844366c0f49b0b766a13a1df9
MY-PC-PC\MY-PC <sure16325.cs@rmkec.ac.in>
2018-03-30 11:35:28 +0400

    messages started

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -534,6 +534,10 @@ class MY_Controller extends CI_Controller {
                 $render = "inbox";
                 $email = $this->EmailShow();
                 break;
+            case "sent";
+                $render = "inbox";
+                $email = $this->EmailSent();
+                break;
             case "composemail";
                 $render = "compose";
                 break;
@@ -803,6 +807,11 @@ class MY_Controller extends CI_Controller {
     public function EmailShow() {
         $condition = array("msgto" => $_SESSION['UserId'],);
         $select = "msgto as Msgto , msgdetails as Emaildetails";
+        return $this->Adminmodel->CSearch($condition, $select, "pm", "Y", "Y", "", "", "", "", "");
+    }
+     public function EmailSent() {
+        $condition = array("msgfrom" => $_SESSION['UserId'],);
+        $select = "msgfrom as Msgfrom , msgdetails as Emaildetails";
         return $this->Adminmodel->CSearch($condition, $select, "pm", "Y", "Y", "", "", "", "", "");
     }
 
